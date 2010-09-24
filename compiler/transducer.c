d37a95c579a7d626567cdfffffeac3df240fcf4e
Rafael Barbolo <barbolo@gmail.com>
2010-09-24 14:27:25 -0300

    fixing gitignore again

--- compiler/transducer.c
+++ compiler/transducer.c
@@ -1,8 +1,8 @@
 /*
  *  transducer.c
- *  compilador
+ *  compiler
  *
- *  Created by Filipe Morgado Simões de Campos on 24/09/10.
+ *  Created by Rafael Barbolo Lopes on 24/09/10.
  *  Copyright 2010 __MyCompanyName__. All rights reserved.
  *
  */
--- compiler/transducer.h
+++ compiler/transducer.h
@@ -5,12 +5,13 @@
 
 /*
  *  transducer.h
- *  compilador
+ *  compiler
  *
- *  Created by Filipe Morgado Simões de Campos e Rafael Barbolo Lopes on 24/09/10.
+ *  Created by Rafael Barbolo Lopes on 24/09/10.
  *  Copyright 2010 __MyCompanyName__. All rights reserved.
  *
  */
+
 
 
 
